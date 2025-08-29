db1ed9df96d4932936789f4e76e66197ec4dc707
hannajas123 <153253585+hannajas123@users.noreply.github.com>
2025-08-29 12:11:58 +0530

    40113

--- home/views.py
+++ home/views.py
@@ -3,5 +3,11 @@ from django.conf import settings
 # Create your views here.
 
 def homepage(request):
-    context= (settings, "RESTAURANT_NAME","My Restaurant","phone_number": settings.RESTAURANT_PHONE)
-    return render(request,"homepage.html",{'restaurant_name':restaurant_name},context)
+    try:
+        context= (settings, "RESTAURANT_NAME","My Restaurant","phone_number": settings.RESTAURANT_PHONE)
+        return render(request,"homepage.html",{'restaurant_name':restaurant_name},context)
+
+    except ValueError as ve:
+        return HttpResponse(f"<h2>Error : {ve}</h2>",  status=404)    
+    except Execption as e: 
+        return HttpResponse("<h2>Something went wrong.Please try again later </h2>", status=500)    
